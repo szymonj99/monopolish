748babd8df7684e4dbe231dab70b6c7007392082
szymonj99 <szymon.j@hotmail.com>
2021-03-18 05:28:01 +0000

    update property square

--- monopolish/CSquareProperty.h
+++ monopolish/CSquareProperty.h
@@ -6,9 +6,6 @@
 
 class CSquareProperty : public CSquare
 {
-private:
-	bool mIsMorgaged = false;
-	uint32_t mIndex = 0;
 public:
 	CSquareProperty(const ESquareType kType, const std::string& kName, const int32_t kCost, const int32_t kRent, const ESquareColour kColour);
 	bool LandOnSquare(std::shared_ptr<CPlayer> player);
